8073139636b8e7142b8666ec8a6d039452532ccb
jamersonsouza <jamersonsouza@outlook.com>
2023-08-09 23:06:12 -0300

    inclusão do javascript pro catalogo

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function (){
+    catalog.events.init();
+});
+
+var catalog = {};
+
+catalog.events = {
+    
+    init: () => {
+    }
+
+}
+catalog.metods = {
+
+    getItensCatalog: () => {
+
+    }
+}
+
+catalog.templates = {
+
+}

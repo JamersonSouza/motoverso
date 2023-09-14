d8ab4a48b4caf806f84e2792ac304f81eb84294b
jamersonsouza <jamersonsouza@outlook.com>
2023-09-13 21:53:56 -0300

    incremento e decremento da quantidade dos itens do catálogo

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,6 +26,7 @@ catalog.metods = {
         $.each(filter, (i, e) => {
             let template = catalog.templates.item.replace(/\${imgs}/g, e.img)
             .replace(/\${name}/g, e.name)
+            .replace(/\${id}/g, e.id)
             .replace(/\${price}/g, e.price.toFixed(2).replace('.', ','));
 
             if(verMais && i >= 4 && i < 12){
@@ -46,6 +47,19 @@ catalog.metods = {
         catalog.metods.getItensCatalog(isAtivo, true);
 
         $('#btnVerMais').addClass('hidden');
+    },
+
+    lessQuantity: (id) => {
+        let quantityCurrent = parseInt($("#qtd-" + id).text());
+        if(quantityCurrent > 0){
+            $("#qtd-" + id).text(quantityCurrent - 1)
+        }
+        console.log('Teste Quantidade: ', quantityCurrent);
+    },
+
+    plusQuantity: (id) => {
+        let quantityCurrent = parseInt($("#qtd-" + id).text());
+        $("#qtd-" + id).text(quantityCurrent + 1)
     }
 }
 
@@ -53,7 +67,7 @@ catalog.templates = {
 
     item:`
     <div class="col-12 col-lg-3 col-md-3 col-sm-6">
-        <div class="card card-item">
+        <div class="card card-item" id="\${id}">
                 <div class="img-catalog text-center">
                     <img src="\${imgs}" width="200px" alt="bauleto para moto pro tork 80 litros">
                         </div>
@@ -64,9 +78,9 @@ catalog.templates = {
                                 <strong>R$ \${price}</strong>
                             </p>
                             <div class="add-car">
-                    <span class="btn-less"><i class="fas fa-minus"></i></span>
-                    <span class="btn-number-itens"><strong>0</strong></span>
-                    <span class="btn-plus"><i class="fas fa-plus"></i></span>
+                    <span class="btn-less" onclick="catalog.metods.lessQuantity('\${id}')"><i class="fas fa-minus"></i></span>
+                    <span class="btn-number-itens" id="qtd-\${id}"><strong>0</strong></span>
+                    <span class="btn-plus" onclick="catalog.metods.plusQuantity('\${id}')"><i class="fas fa-plus"></i></span>
                     <span class="btn btn-add"><i class="fa fa-shopping-bag"></i></span>
                 </div>
             </div>
